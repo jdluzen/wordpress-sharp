fc5de154971cf8649f3787779f695d98b881f028
Joe Dluzen <jdluzen@gmail.com>
2013-10-19 12:07:57 -0400

    Add build project.

---
+++ WordPress-Sharp.Web.Build/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace WordPress_Sharp.Web.Build
+{
+    public static class Extensions
+    {
+        public static string Concat(this List<string> list, string delim, string surround)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < list.Count; i++)
+            {
+                builder.AppendFormat("{0}{1}{0}", surround, list[i]);
+                if (i < list.Count - 1)
+                    builder.Append(delim);
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ WordPress-Sharp.Web.Build/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace WordPress_Sharp.Web.Build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> phpcArgs = new List<string>
+            {
+                "/target:web",
+                string.Format("/recurse:{0}", Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "..", "WordPress-Sharp.Web")),
+
+            };
+            Process phpc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "phpc",
+                    Arguments = phpcArgs.Concat(" ", "\""),
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                }
+            };
+            Console.WriteLine("Building with {0}", phpc.StartInfo.Arguments);
+            phpc.Start();
+            using (phpc.StandardOutput)
+            {
+                File.WriteAllText("build.log", phpc.StandardOutput.ReadToEnd());
+            }
+            using (phpc.StandardError)
+            {
+                File.WriteAllText("build.error.log", phpc.StandardError.ReadToEnd());
+            }
+            phpc.WaitForExit();
+            int code = phpc.ExitCode;
+            if (code > 0)
+                Console.WriteLine("Exited with {0}", code);
+            else
+                Console.WriteLine("Success!");
+        }
+    }
+}
